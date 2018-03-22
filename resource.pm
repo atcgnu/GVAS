a3ce26c7125fa5178188901c0dce7f99c028d2aa
atcgnu <atcgnu@gmail.com>
2018-03-22 17:53:02 +0800

    add filter-af

---
+++ resource.pm
@@ -0,0 +1,15 @@
+use FindBin qw($Bin);
+use lib ("$FindBin::RealBin");
+
+our %env_vars = (
+    PATH => "/hwfssz1/ST_MCHRI/DISEASE/DEV/pipeline/rdresearch/v2.0.0/bin:\$PATH",
+    PERL5LIB => "/hwfssz1/ST_MCHRI/DISEASE/DEV/pipeline/rdresearch/v2.0.0/software/perl-5.24.0/lib:/hwfssz1/ST_MCHRI/DISEASE/DEV/pipeline/rdresearch/v2.0.0/software/vcftools_0.1.12b/lib/perl5/site_perl"
+);
+
+our %_wftool_ = (
+	perl => "/home/shenyulan/Workspace/DEV/pub/software/perl5240/bin/perl",
+);
+
+our %_data_ = (
+	dbpp => "/home/shenyulan/Workspace/DEV/pipeline/rdresearch/v3.0.0/data/GRCh37/dbpp",
+);
