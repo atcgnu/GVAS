a4eb4f36cc5a41c8d88f27798acd315f93a35d57
atcgnu <atcgnu@gmail.com>
2018-03-22 21:35:06 +0800

    add filter-consequnce.pl

---
+++ t.sh
@@ -0,0 +1,1 @@
+time /home/shenyulan/Workspace/DEV/pub/software/perl5240/bin/perl /home/shenyulan/Workspace/DEV/pipeline/GVAS/filter-af.pl /home/shenyulan/Workspace/DEV/pipeline/rdresearch/v3.0.0/data/GRCh37/dbsnp/t/F08.gatkHC.snp.indel.VQSR.vcf out /home/shenyulan/Workspace/DEV/pipeline/rdresearch/v3.0.0/data/GRCh37/dbsnp/t/screen.config
